46b7a2346e7e701ac73c0878128bb93ed2da3cb5
crackpotwolf <artyushinvl@gmail.com>
2021-05-21 19:51:39 +0300

    Добавлены шаблоны

---
+++ BackendFrontend/Hackathon-TEK/Hackathon-TEK/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hackathon_TEK.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public virtual IActionResult HomeView()
+        {
+            return PartialView("_HomePartial"); 
+        }
+
+
+
+
+    }
+}
