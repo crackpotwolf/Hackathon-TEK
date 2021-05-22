3b15e7cfd363f4f48409093c7a16efe14487e5ee
Влад Трубицин <trubitsin.vlad@outlook.com>
2021-05-22 16:07:02 +0300

    Отрисовка регионов на карте со случайными данными

--- BackendFrontend/Hackathon-TEK/Hackathon-TEK/Controllers/HomeController.cs
+++ BackendFrontend/Hackathon-TEK/Hackathon-TEK/Controllers/HomeController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Hackathon_TEK.Interfaces;
+using Hackathon_TEK.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Hackathon_TEK.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IRepository<Region> _regionsRepos;
+
+        public HomeController(IRepository<Region> regionsRepos)
+        {
+            this._regionsRepos = regionsRepos;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -20,13 +30,15 @@ namespace Hackathon_TEK.Controllers
         {
             try
             {
-                return new Dictionary<string, int>()
-                {
-                    {"RU-KAM", 0},
-                    {"RU-BRY", 1},
-                    {"RU-ALT", 3},
-                    {"RU-VGG", 6}
-                };
+                var regions = _regionsRepos.GetListQuery().Select(p => new KeyValuePair<string, int>(p.MapId,new Random().Next(0,6)));
+                return regions.ToDictionary(p=>p.Key, p=>p.Value);
+                //return new Dictionary<string, int>()
+                //{
+                //    {"RU-KAM", 0},
+                //    {"RU-BRY", 1},
+                //    {"RU-ALT", 3},
+                //    {"RU-VGG", 6}
+                //};
             }
             catch (System.Exception)
             {
