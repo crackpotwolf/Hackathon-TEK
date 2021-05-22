f31e69180f92d83253b9032ccbc58bd019555ae1
Влад Трубицин <trubitsin.vlad@outlook.com>
2021-05-22 23:56:36 +0300

    Модели и контроллеры для записи результатов анализа

---
+++ BackendFrontend/Hackathon-TEK/Hackathon-TEK/Migrations/20210522205229_analyzes.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Hackathon_TEK.Migrations
+{
+    public partial class analyzes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Analyzes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Date = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Probability = table.Column<double>(type: "double precision", nullable: false),
+                    ObjectType = table.Column<string>(type: "text", nullable: true),
+                    EventType = table.Column<string>(type: "text", nullable: true),
+                    IsDelete = table.Column<bool>(type: "boolean", nullable: false),
+                    RegionId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Analyzes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Analyzes_Regions_RegionId",
+                        column: x => x.RegionId,
+                        principalTable: "Regions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Analyzes_RegionId",
+                table: "Analyzes",
+                column: "RegionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Analyzes");
+        }
+    }
+}
---
+++ BackendFrontend/Hackathon-TEK/Hackathon-TEK/Models/Analyze.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hackathon_TEK.Models
+{
+    public class Analyze : RegionConnectedObject
+    {
+        public DateTime Date { get; set; }
+
+        public double Probability { get; set; }
+
+        public string ObjectType { get; set; }
+
+        public string EventType { get; set; }
+    }
+}
