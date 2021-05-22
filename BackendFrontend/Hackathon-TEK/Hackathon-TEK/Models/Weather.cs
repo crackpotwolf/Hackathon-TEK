b16bfa736bdb6e49e513d607cbf5b061ac32ec81
Влад Трубицин <trubitsin.vlad@outlook.com>
2021-05-22 04:08:31 +0300

    Обновления метода выгрузки регионов

---
+++ BackendFrontend/Hackathon-TEK/Hackathon-TEK/Models/Weather.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hackathon_TEK.Models
+{
+    public class Weather : RegionConnectedObject
+    {
+        public string StationName { get; set; }
+
+        public string StationRegion { get; set; }
+
+        public double StationLat { get; set; }
+
+        public string StationLon { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public double TempMin0 { get; set; }
+
+        public double TempAverage0 { get; set; }
+
+        public double TempMax0 { get; set; }
+
+        public double TempDifNorm0 { get; set; }
+
+        public double Percipitation { get; set; }
+
+        public double TempAverage { get; set; }
+
+        public double PressureMax { get; set; }
+
+        public double HumidityMax { get; set; }
+
+        public double WindSpeedMax { get; set; }
+
+        public double WindDegMax { get; set; }
+
+        public double CloudsMax { get; set; }
+
+    }
+}
