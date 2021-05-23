7d09ebb125a738fd6217319cf213231e0e83f3d9
kirill20123456 <dereguzov1@yandex.ru>
2021-05-23 05:00:15 +0300

    Weather post

--- BackendFrontend/Hackathon-TEK/Hackathon-TEK/Controllers/API/WeatherController.cs
+++ BackendFrontend/Hackathon-TEK/Hackathon-TEK/Controllers/API/WeatherController.cs
@@ -23,7 +23,7 @@ namespace Hackathon_TEK.Controllers.API
         protected readonly IRepository<Reason> _reasonRepository;
         private readonly ILogger<IndexModel> _logger;
 
-        public WeatherController(ILogger<IndexModel> logger, IRepository<Weather> repository, 
+        public WeatherController(ILogger<IndexModel> logger, IRepository<Weather> repository,
             IRepository<Region> regionRepository, IRepository<Reason> reasonRepository)
         {
             _logger = logger;
@@ -46,12 +46,13 @@ namespace Hackathon_TEK.Controllers.API
             {
                 var res = _repository.GetList().Join(
                     _reasonRepository.GetList(),
-                    w=>new { w.Date.Date, w.RegionId},
-                    r=>new { r.Date.Date, r.RegionId},
-                    (w,r)=> {
+                    w => new { w.Date.Date, w.RegionId },
+                    r => new { r.Date.Date, r.RegionId },
+                    (w, r) =>
+                    {
                         return new
                         {
-                            Id=w.Id,
+                            Id = w.Id,
                             StationId = w.StationId,
                             StationName = w.StationName,
                             StationRegion = w.StationRegion,
@@ -70,12 +71,12 @@ namespace Hackathon_TEK.Controllers.API
                             WindDegMax = w.WindDegMax,
                             CloudsMax = w.CloudsMax,
                             RegionId = w.RegionId,
-                            ReasonId=(int?)r.Id
+                            ReasonId = (int?)r.Id
                         };
                     }).ToList();
                 var resIds = res.Select(p => p.Id).ToList();
                 var excepted = _repository.GetListQuery().Where(p => !resIds.Any(t => t == p.Id))
-                    .Select(w=> new
+                    .Select(w => new
                     {
                         Id = w.Id,
                         StationId = w.StationId,
@@ -137,10 +138,10 @@ namespace Hackathon_TEK.Controllers.API
         {
             try
             {
-                var dateTime = DateTime.ParseExact(date,"dd.MM.yyyy", CultureInfo.InvariantCulture);
+                var dateTime = DateTime.ParseExact(date, "dd.MM.yyyy", CultureInfo.InvariantCulture);
                 return Ok(_repository.GetListQuery().FirstOrDefault(p => p.Date == dateTime && p.Region.MapId == regionMapId));
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 _logger.LogError($"Ошибка: {ex.Message}");
 
@@ -156,8 +157,8 @@ namespace Hackathon_TEK.Controllers.API
         {
             try
             {
-                var region = _regionRepository.GetListQuery().FirstOrDefault(p=>p.Name==name);
-                return Ok(_repository.GetListQuery().Where(p => p.Date >= start && p.Date<=end && p.RegionId==region.Id));
+                var region = _regionRepository.GetListQuery().FirstOrDefault(p => p.Name == name);
+                return Ok(_repository.GetListQuery().Where(p => p.Date >= start && p.Date <= end && p.RegionId == region.Id));
             }
             catch (Exception ex)
             {
@@ -180,8 +181,9 @@ namespace Hackathon_TEK.Controllers.API
             try
             {
                 var results = new List<Weather>();
-                foreach (var obj in data) {
-                    
+                foreach (var obj in data)
+                {
+
                     var region = _regionRepository.GetListQuery().First(p => p.Name == obj.Value.station_region);
                     foreach (var item in obj.Value.weather_dates)
                     {
@@ -206,8 +208,73 @@ namespace Hackathon_TEK.Controllers.API
                         res.WindDegMax = item.Value.wind_deg_max;
                         res.WindSpeedMax = item.Value.wind_speed_max;
                         results.Add(res);
+                    }
                 }
+                var weather = _repository.GetList().Join(
+                    results,
+                    p => new { p.StationId, p.Date },
+                    d => new { d.StationId, d.Date },
+                    (p, d) =>
+                    {
+                        d.Id = p.Id;
+                        return d;
+                    })
+                    .ToList();
+                var excepted = results.ExceptBy(weather, p => new { p.StationId, p.Date }).ToList();
+                _repository.AddRange(excepted);
+
+                _repository.UpdateRange(weather);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Ошибка: {ex.Message}");
+
+                return StatusCode(500, $"Не удалось ...");
+            }
+        }
+
+
+        /// <summary>
+        /// Запись списка данных о погоде
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("Post")]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(Exception), 400)]
+        public IActionResult Post(WeatherDataVM data)
+        {
+            try
+            {
+                var results = new List<Weather>();
+                var region = _regionRepository.GetListQuery().First(p => p.Name == data.station_region);
+                foreach (var item in data.weather_dates)
+                {
+                    var res = new Weather();
+                    if (region != null)
+                        res.RegionId = region.Id;
+                    res.Date = new DateTime(Int32.Parse(item.Key.Split(".")[2]), Int32.Parse(item.Key.Split(".")[1]), Int32.Parse(item.Key.Split(".")[0]));
+                    //res.StationId = Int32.Parse(data.Key);
+                    res.CloudsMax = item.Value.clouds_max;
+                    res.HumidityMax = item.Value.humidity_max;
+                    res.Percipitation = item.Value.precipitation_0;
+                    res.PressureMax = item.Value.pressure_max;
+                    res.StationLat = data.station_coordinate[0];
+                    res.StationLon = data.station_coordinate[1];
+                    res.StationName = data.station_name;
+                    res.StationRegion = data.station_region;
+                    res.TempAverage = item.Value.temp_average;
+                    res.TempAverage0 = item.Value.temp_average_0;
+                    res.TempDifNorm0 = item.Value.temp_dif_norm_0;
+                    res.TempMax0 = item.Value.temp_max_0;
+                    res.TempMin0 = item.Value.temp_min_0;
+                    res.WindDegMax = item.Value.wind_deg_max;
+                    res.WindSpeedMax = item.Value.wind_speed_max;
+                    results.Add(res);
                 }
+
                 var weather = _repository.GetList().Join(
                     results,
                     p => new { p.StationId, p.Date },
